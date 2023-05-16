c7d022bb819a868ddbbc527e013831b202e69e3a
merhatsidk <merhatsidk29@gmail.com>
2023-05-16 01:11:07 -0500

    welcome note updated

--- src/main/java/com/example/devopsintegeration/DevopsIntegerationApplication.java
+++ src/main/java/com/example/devopsintegeration/DevopsIntegerationApplication.java
@@ -2,12 +2,19 @@ package com.example.devopsintegeration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsIntegerationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsIntegerationApplication.class, args);
-	}
+    @GetMapping
+    public String message(){
+        return "welcome this is merha!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsIntegerationApplication.class, args);
+    }
 
 }

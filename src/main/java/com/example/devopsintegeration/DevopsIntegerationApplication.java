63ff5c79b4410f05902616b16667ec5c0a744df4
merhatsidk <merhatsidk29@gmail.com>
2023-05-16 01:03:53 -0500

    first commit

---
+++ src/main/java/com/example/devopsintegeration/DevopsIntegerationApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsintegeration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsIntegerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegerationApplication.class, args);
+	}
+
+}
